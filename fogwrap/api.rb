60474031b29e2d6a0bd5d6b32d2e58e182dd9782
Victor Lowther <victor.lowther@gmail.com>
2015-10-26 10:42:36 -0500

    api can add ports and SSH pubkeys for remote access

--- fogwrap/api.rb
+++ fogwrap/api.rb
@@ -10,14 +10,16 @@ require 'diplomat'
 class Servers
   extend Jimson::Handler
 
-  def create(endpoint, args)
+  def create(endpoint, node_id, args)
+    log("Creating node #{node_id}")
     ep = get_endpoint(endpoint)
     fixed_args = fix_hash(args)
     fixed_args[:private_key_path] = File.expand_path("~/.ssh/fog_rsa")
     fixed_args[:public_key_path] = File.expand_path("~/.ssh/fog_rsa.pub")
     fixed_args[:user] = "root"
     server = ep.servers.create(fixed_args)
-    Diplomat::Kv.put("fogwrap/create/#{endpoint["provider"]}/#{server.id}",endpoint.to_json)
+    log("Created server #{server.to_json}")
+    Diplomat::Kv.put("fogwrap/create/#{node_id}/#{server.id}",endpoint.to_json)
     server
   end
 
@@ -31,13 +33,59 @@ class Servers
     ep.servers.get(id)
   end
 
+  def reboot(endpoint, id)
+    log("Rebooting server #{id}")
+    get(endpoint,id).reboot
+  end
+
   def delete(endpoint,id)
+    log("Deleting server #{id}")
     ep = get_endpoint(endpoint)
     server = ep.servers.get(id)
     server.destroy
   end
 
+  def register(endpoint, user, keys)
+    log("Registering endpoint #{endpoint} using #{keys}")
+    ep = get_endpoint(endpoint)
+    keys["fogwrap"] = File.read(File.expand_path("~/.ssh/fog_rsa.pub")).strip
+    case endpoint["provider"]
+    when "AWS"
+      keys.each do |k,v|
+        next if ep.key_pairs.get(k)
+        log "Registering key #{k}: #{v}"
+        res = ep.import_key_pair(k,v)
+        if res.body['keyFingerprint'] && !res.body['keyFingerprint'].empty?
+          log "Key #{k} registered as #{res.body['keyFingerprint']}"
+        else
+          log "Registration of #{k} failed"
+        end
+      end
+      sg = ep.security_groups.get('default')
+      # make sure port 22 is open in the first security group
+      unless sg.ip_permissions.find do |ip_permission|
+               ip_permission['ipRanges'].find { |ip_range| ip_range['cidrIp'] == '0.0.0.0/0' } &&
+                 ip_permission['fromPort'] == 22 &&
+                 ip_permission['ipProtocol'] == 'tcp' &&
+                 ip_permission['toPort'] == 22
+             end
+        log "Allowing SSH access"
+        sg.authorize_port_range(22..22)
+      else
+        log "SSH access already enabled"
+      end
+      
+    else
+      raise "No idea how to handle #{endpoint["provider"]}"
+    end
+  end
+
   private
+
+  def log(line)
+    STDOUT.puts(line)
+  end
+                
   def fix_hash(h)
     res = {}
     h.each_key do |k|
