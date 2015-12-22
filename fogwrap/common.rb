1284835fa053ce2a8abad4c66f19d14fd5417348
Greg Althaus <galthaus@austin.rr.com>
2015-12-22 12:55:55 -0600

    Add a fake mode that simulates walking the process and a test client to read the list.
    Also - start to get pieces in place to generate waits, but not
    in yet.

--- fogwrap/common.rb
+++ fogwrap/common.rb
@@ -11,7 +11,150 @@ def fix_hash(h)
   res
 end
 
+#
+# Debug / Mock to test scale
+#
+# ep['debug'] = {
+#   'host_ip'    => 'ip of fake host - should be sshable with rebar key - use docker node',
+#   'boot_delay_time' => 300,
+#   'ssh_delay_time' => 300,
+# }
+#
+
+class FakeDriver
+   @@servers = nil
+   @@keypairs = nil
+
+   def initialize(ep)
+     @end_point = ep
+     unless @@servers
+       @@servers = Servers.new(self)
+     end
+     unless @@keypairs
+       @@keypairs = KeyPairs.new
+     end
+     @ss = @@servers
+     @kp = @@keypairs
+   end
+
+   class KeyPairs < Hash
+     def get(id)
+       self[id]
+     end
+   end
+
+   class Servers < Hash
+     def initialize(pp)
+       @parent = pp
+     end
+     def get(id)
+       s = self[id]
+       unless s
+         s = Server.new(self, { :tags => { "rebar:node-id" => id } })
+         self[id] = s
+       end
+       s
+     end
+
+     def create(opts)
+       s = Server.new(self, opts)
+       self[s.name] = s
+       s
+     end
+
+     def get_endpoint_address
+       @parent.endpoint_data['debug']['host_ip']
+     end
+   end
+
+   class Server
+     def initialize(parent, opts)
+       @parent = parent
+       @hostname = opts[:hostname]
+       @nameid = "#{opts[:tags]["rebar:node-id"]}"
+     end
+
+     def reboot
+       true
+     end
+
+     def id
+       @nameid
+     end
+
+     def name
+       @nameid
+     end
+
+     def destroy
+       @parent.delete(name)
+       true
+     end
+
+     def ready?
+       true
+     end
+
+     def private_key_path=(s)
+     end
+
+     def username=(s)
+     end
+
+     def sshable?
+       true
+     end
+
+     def ssh(command)
+       true
+     end
+
+     def scp(path, command)
+       true
+     end
+
+     def public_ip_address
+       @parent.get_endpoint_address
+     end
+   end
+
+   class Status
+     def initialize(res)
+       @result = res
+     end
+
+     def status
+       @result
+     end
+   end
+
+   def servers
+     @ss
+   end
+
+   def endpoint_data
+     @end_point
+   end
+
+   def key_pairs
+     @kp
+   end
+
+   def import_key_pair(name, key)
+     @kp[name] = key
+     Status.new(200)
+   end
+
+   def region
+     'us-west-1'
+   end
+
+end
+
+
 def get_endpoint(ep)
+  return FakeDriver.new(ep) if ep['debug']
+
   case ep['provider']
   when 'AWS' then Fog::Compute.new(fix_hash(ep))
   when 'Google'
